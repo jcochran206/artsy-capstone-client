7895dde4c4dd3b946b23db15c5d422fed5875dc6
Sacha Sedriks <sachased@gmail.com>
2020-11-03 14:43:00 -0800

    Minor Updates to Post & SignIn so app renders

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 
+// temp
+import HomeFeed from './components/HomeFeed'
+import Post from './components/Post'
+import PostsList from './components/PostsList'
+
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+    <BrowserRouter>
+
+        {/* temp, just to see if certain components render in DOM */}
+        {/* <Post /> */}
+        {/* <PostsList /> */}
+        {/* <HomeFeed /> */}
+
+        <App />
+        
+    </BrowserRouter>,
+    document.getElementById('root')
 );
 
