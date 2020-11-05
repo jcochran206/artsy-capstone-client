da066b0b6869df219c0e26734a979b9ee3acf20d
Sacha Sedriks <sachased@gmail.com>
2020-11-04 17:09:53 -0800

    Update auth api service

---
+++ src/services/auth-api-service.js
@@ -0,0 +1,20 @@
+import config from '../config'
+
+const AuthApiService = {
+    postLogin(credentials) {
+        return fetch(`${config.API_ENDPOINT}/auth/login`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(credentials),
+        })
+            .then(res =>
+                (!res.ok)
+                    ? res.json().then(e => Promise.reject(e))
+                    : res.json()
+            )
+    },
+}
+
+export default AuthApiService
