01641043f27652ab42ecc598ba8173a072f2329b
Sacha Sedriks <sachased@gmail.com>
2020-11-10 21:40:09 -0800

    Update some styling. Very much WIP.

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -3,12 +3,23 @@ import { format } from 'date-fns'
 import { Redirect } from 'react-router-dom'
 import PostImage from './PostImage'
 
+import {
+    CgProfile,
+} from 'react-icons/cg'
+
+import {
+    RiEditLine,
+    RiEditFill,
+    RiHeartLine,
+    RiDislikeLine,
+} from 'react-icons/ri'
+
 Posts.defaultProps = {
-    username: 'how to join tables?',
+    username: ':username',
     avatarUrl: 'missing from db',
     date_created: '2000-01-01T00:00:00.000Z',
     repost: 'missing from db',
-    repostedBy: 'missing from db',
+    repostedBy: ':username',
 }
 
 
@@ -42,28 +53,40 @@ export default function Posts(props){
             {repost && <p className='post-repost-info'>reposted by <Link to={`/profile/${repostedBy}`}>{repostedBy}</Link></p>}
             <div className="post">
                 <div className="post-attribution">
-                    <img src={avatarUrl} alt="avatar" />
+                    {/* <img src={avatarUrl} alt="avatar" /> */}
+                    <CgProfile />
                     <p><Link to={`/profile/${username}`}>{username}</Link></p>
                 </div>
                 <div className='post-img'>
                     <PostImage src={imageUrl} />
                 </div>
                 <div className="post-actions">
-                    <button onClick={() => handleLike()}>Like</button>
+                    <button onClick={() => handleLike()}><RiHeartLine /></button>
                     <button onClick={() => postComment()}>Comment</button>
                     {/* TODO: Conditional ==> if (user's post), Edit button, else Repost button */}
                     <button onClick={() => handleRepost()}>Repost</button>
                     {/* <button onClick={() => handleEdit()}>Edit</button> */}
-                    <Link to={`/edit/${id}`}>Edit</Link>
+                    <Link to={`/edit/${id}`}><RiEditLine /></Link>
                 </div>
                 <div className='post-info'>
-                    <p className="title">{title}</p>
-                    <p className="description">{description}</p>
-                    <p className="timestamp">{format(Date.parse(date_created), 'MMM do yyyy')}</p>
+                <ul>
+                    <li className="title">{title}</li>
+                    <li className="description">{description}</li>
+                    <li className="timestamp">{format(Date.parse(date_created), 'MMM do yyyy')}</li>
+                </ul>
+                    
                     <div className='comments-container'>
                         {/* <p>{comments}</p> */}
                     </div>
                 </div>
+                {/* <div className='post-info'>
+                    <p className="title">{title}</p>
+                    <p className="description">{description}</p>
+                    <p className="timestamp">{format(Date.parse(date_created), 'MMM do yyyy')}</p>
+                    <div className='comments-container'>
+                        <p>{comments}</p>
+                    </div>
+                </div> */}
             </div>
         </section>
     )
