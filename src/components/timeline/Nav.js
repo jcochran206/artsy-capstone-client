7e9afbd374d73e0b0d97bdfa424e4711ee22280b
Sacha Sedriks <sachased@gmail.com>
2020-11-11 16:01:21 -0800

    Moved Logout button from Nav to Profile

--- src/components/timeline/Nav.js
+++ src/components/timeline/Nav.js
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom'
-import TokenService from '../../services/token-service'
 import UserService from '../../services/user-service'
 
 import {
@@ -18,21 +17,10 @@ import {
     CgProfile,
 } from 'react-icons/cg'
 
-import {
-    IoMdLogin,
-    IoMdLogOut,
-} from 'react-icons/io'
-
-
 
 export default function Nav(){
 
-    // TODO: pull in actual username
     const username = UserService.getUser('username')
-    const handleLogoutClick = () => {
-        UserService.clearUser()
-        TokenService.clearAuthToken()
-    }
     
     return(
         <nav role="navigation">
@@ -41,10 +29,8 @@ export default function Nav(){
             <li aria-label="Home" className= "selected"><Link to='/feed/home'><RiHome3Line /></Link></li>
             <li aria-label="Explore" className=""><Link to='/feed/explore'><RiEyeLine /></Link></li>
             <li aria-label="Post" className=""><Link to='/upload'><RiAddCircleLine /></Link></li>
-            <li aria-label="Profile" className=""><Link to={`/profile/${username}`}><CgProfile /></Link></li> {/*user will be id of user*/}
             <li aria-label="Search" className=""><Link to='/search'><RiSearchLine /></Link></li> 
-            <li aria-label="Logout" id="logout" className=""><Link to='/' onClick={handleLogoutClick}>{username} Logout</Link></li>
-            {/* <li aria-label="Logout" className=""><Link to='/' onClick={handleLogoutClick}><IoMdLogOut /></Link></li> */}
+            <li aria-label="Profile" className=""><Link to={`/profile/${username}`}><CgProfile /></Link></li> {/*user will be id of user*/}
         </ul>
     </nav>
     )
