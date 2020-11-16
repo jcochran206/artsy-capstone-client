40989932b3a849fa2888e418fe33d44da33fb091
Sacha Sedriks <sachased@gmail.com>
2020-11-15 17:06:35 -0800

    Update /feed/home with intro text when empty

--- src/components/timeline/Feed.js
+++ src/components/timeline/Feed.js
@@ -1,7 +1,8 @@
+import { useEffect, useState } from 'react'
 import Post from '../post/Post'
 import ApiService from '../../services/api-service'
-import { useEffect, useState } from 'react'
 import UserService from '../../services/user-service'
+import { Link } from 'react-router-dom'
 
 export default function Feed(props){
     const [posts, set] = useState([])
@@ -9,18 +10,31 @@ export default function Feed(props){
     const myId = UserService.getUser('userid')
 
     useEffect(() => {
-        if(type === 'home'){
+        if (type === 'home'){
             set([])
         }
-        if(type === 'explore'){
+        if (type === 'explore'){
             ApiService.getExplorePosts()
             .then(res => set(res))    
         }
 
     }, [myId, type, set])
+
     const displayPost = () => {
+        if (type === 'home' && posts.length === 0) {
+            return (
+                <div className="home__intro">
+                    <h1>Welcome!</h1>
+                    <p>This is your new artsy homepage.</p> 
+                    <p>Eventually it will display a personalized feed of posts by people you follow.</p> 
+                    <p>It's currently empty because you aren't following anyone yet... but that's easy to remedy.</p> 
+                    <p><Link to='/feed/explore' className='signin__buttonlink'>Head over to the artsy explore feed.</Link> There you will see posts from across the artsy community and you can begin following some people!</p>
+                    <p>Once you do, come back home...</p>
+                </div>
+            )
+        }
         return posts.map((x, i) => {
-            return <Post {...x} key={i} history={props.history}/> //timeline/explore posts
+            return <Post {...x} key={i} history={props.history}/> //feed of posts
         })
     }
 
