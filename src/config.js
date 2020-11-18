b54184e57d2a6fd5ea77e557a01798d26bb74dde
Sacha Sedriks <sachased@gmail.com>
2020-11-18 14:49:48 -0800

    Responsive design adjustment to width of post wrapper

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    API_ENDPOINT: `http://localhost:8000`, 
-    // API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || `http://localhost:8000`, 
+    // API_ENDPOINT: `http://localhost:8000`, 
+    API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || `http://localhost:8000`, 
     TOKEN_KEY: 'artsy-client-auth-token',
     // API_KEY: process.env.REACT_APP_API_KEY || '',
 }
