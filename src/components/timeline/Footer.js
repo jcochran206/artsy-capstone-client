af9c7b9796da242e813c162df454ce964049c0a0
Sacha Sedriks <sachased@gmail.com>
2020-11-19 20:21:17 -0800

    footer link target _blank

--- src/components/timeline/Footer.js
+++ src/components/timeline/Footer.js
@@ -4,7 +4,7 @@ import GitHubIcon from '../icons/GitHubIcon'
 export default function Footer(props) {
     return (
         <footer className="footer" role="contentinfo"> 
-            <a href="https://github.com/jcochran206/artsy-capstone-client" alt="GitHub"><GitHubIcon className='icon' />
+            <a href="https://github.com/jcochran206/artsy-capstone-client" target="_blank" alt="GitHub"><GitHubIcon className='icon' />
             <p>by Michella, Jonathan, Jarred, and Sacha.</p></a> 
         </footer>
     )
